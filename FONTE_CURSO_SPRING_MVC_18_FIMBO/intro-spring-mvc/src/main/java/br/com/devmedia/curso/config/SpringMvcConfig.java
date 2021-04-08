cc6656d77894689dcbb71a52afed4711934549a7
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-04-07 22:48:33 -0300

    3. Configurando o ambiente de desenvolvimento

--- FONTE_CURSO_SPRING_MVC_18_FIMBO/intro-spring-mvc/src/main/java/br/com/devmedia/curso/config/SpringMvcConfig.java
+++ FONTE_CURSO_SPRING_MVC_18_FIMBO/intro-spring-mvc/src/main/java/br/com/devmedia/curso/config/SpringMvcConfig.java
@@ -1,24 +1,17 @@
 package br.com.devmedia.curso.config;
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-import br.com.devmedia.curso.web.conversor.TipoSexoConverter;
-
 @Configuration
-public class SpringMvcConfig extends WebMvcConfigurerAdapter {
+public class SpringMvcConfig {
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/static/**").addResourceLocations("/WEB-INF/resources/bootstrap/");
-	}
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry.addResourceHandler("/static/**").addResourceLocations("/WEB-INF/resources/bootstrap/");
+//	}
 
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
@@ -29,17 +22,17 @@ public class SpringMvcConfig extends WebMvcConfigurerAdapter {
 		return resolver;
 	}
 
-	@Override
-	public void addFormatters(FormatterRegistry registry) {
-		registry.addConverter(new TipoSexoConverter());
-	}
+//	@Override
+//	public void addFormatters(FormatterRegistry registry) {
+//		registry.addConverter(new TipoSexoConverter());
+//	}
 	
-	@Bean
-	public MessageSource messageSource() {
-		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
-		source.setBasename("messages");
-		return source;
-	}
+//	@Bean
+//	public MessageSource messageSource() {
+//		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+//		source.setBasename("messages");
+//		return source;
+//	}
 	
 	
 }
